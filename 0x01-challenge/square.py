4ecab0b80b4bb0bccaeb89c8b654cd26be0011f0
Gzoref <gzoref@gmail.com>
2020-05-19 13:56:57 -0400

    [Debug] 1. My Square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 class square():
+    '''
+    Square class
+    '''
 
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
+        '''
+        Init
+        '''
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -13,9 +19,15 @@ class square():
         return self.width * self.width
 
     def permiter_of_My_Square(self):
+        '''
+        Perimeter
+        '''
         return (self.width * 4)
 
     def __str__(self):
+        '''
+        to string
+        '''
         return "{}/{}".format(self.width, self.width)
 
 
